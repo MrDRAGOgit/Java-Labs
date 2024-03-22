2005c4329c30dbc7d2fb8fd81dec8a8ef4d25074
MrDRAGO <plusha2001@yandex.ru>
2024-03-22 14:41:30 +0300

    Create and sorted an arr

--- java_lab2/src/Main.java
+++ java_lab2/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        Author bradbury = new Author("R.Bradbury");
+        Book book3 = new Book("451 Fharengeit", 327, bradbury);
+        Book book1 = new Book("Dandelion Wine", 251, bradbury);
+        Author tolkien = new Author("J.R.R.Tolkien");
+        Book book2 = new Book("Silmarileon", 1241, tolkien);
+        Book[] books = new Book[]{book1, book2, book3};
+        Arrays.sort(books);
+        System.out.println(Arrays.toString(books));
     }
 }
